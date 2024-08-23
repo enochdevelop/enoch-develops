3a5e1b82015a16a5e2f4e2a71e5a8d0e4c9e5e26
Enoch Ajagbe <52798209+enochdevelop@users.noreply.github.com>
2024-08-23 03:35:02 -0400

    Update MenuButton.js

--- src/MenuButton.js
+++ src/MenuButton.js
@@ -58,7 +58,6 @@ function MenuButton() {
                 
                 <ul>
                     <DropdownItem tag={Link} href="/" img={home} text={"Home"} />
-                    <DropdownItem tag={Link} href="/AboutMe" img={user} text={"About"} />
                     <DropdownItem tag={Link} href="/ArProjects" img={work} text={"Work"} />
                 </ul>
             </div>
